2dbb36737e130a6a77bc92ad913fbd37d1ecb5a1
github username <github email address>
2019-02-02 22:15:47 -0500

    Added store, actions, reducers folders

---
+++ src/store/reducers/places.js
@@ -0,0 +1,14 @@
+const initalState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+
+  }
+}
+
+export default reducer;
