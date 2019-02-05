8bb751ed3680f9867104b5da5b10acc5f1a01d45
github username <github email address>
2019-02-05 08:29:08 -0500

    Successfully added React Navigation

--- android/app/src/main/java/com/planner/MainApplication.java
+++ android/app/src/main/java/com/planner/MainApplication.java
@@ -15,37 +15,6 @@ import java.util.List;
 
 public class MainApplication extends NavigationApplication {
 
-  // private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-  //   @Override
-  //   public boolean getUseDeveloperSupport() {
-  //     return BuildConfig.DEBUG;
-  //   }
-  //
-  //   @Override
-  //   protected List<ReactPackage> getPackages() {
-  //     return Arrays.<ReactPackage>asList(
-  //         new MainReactPackage(),
-  //         new VectorIconsPackage()
-  //     );
-  //   }
-  //
-  //   @Override
-  //   protected String getJSMainModuleName() {
-  //     return "index";
-  //   }
-  // };
-  //
-  // @Override
-  // public ReactNativeHost getReactNativeHost() {
-  //   return mReactNativeHost;
-  // }
-  //
-  // @Override
-  // public void onCreate() {
-  //   super.onCreate();
-  //   SoLoader.init(this, /* native exopackage */ false);
-  // }
-
   @Override
   public boolean isDebug() {
     // Make sure you are using BuildConfig from your own application
